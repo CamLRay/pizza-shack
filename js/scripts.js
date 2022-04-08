365375b3427711c7ee7ccfa1dbb481ef4f502903
Sploob <crayiphone@gmail.com>
2022-04-08 09:16:41 -0700

    pass first test for priceCalc method

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,18 @@ function Pizza(toppings, size) {
   this.toppings = toppings;
   this.size = size;
 }
+
+Pizza.prototype.priceCalc = function(pizza) {
+  let price;
+  if(this.size === "10 inch") {
+    price = 11;
+  } else if (this.size === "12 inch") {
+    price = 13;
+  } else if (this.size === "14 inch") {
+    price = 16;
+  }
+  return price;
+}
+
+let pizza = new Pizza(["pepperoni"], "15 inch");
+console.log(pizza.priceCalc());
