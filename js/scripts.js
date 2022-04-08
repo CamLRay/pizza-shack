1f1235058b6e072641ff3089bba8bef715222801
Sploob <crayiphone@gmail.com>
2022-04-08 11:28:37 -0700

    generate options based on menu object

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,8 @@ function Menu(toppings, sizes) {
   this.sizes = sizes;
 }
 
+let menu = new Menu(["Pepperoni", "Bacon", "Sausage", "Canadian Bacon", "Mushrooms", "Black Olives", "Green Pepper", "Garlic", "Onions", "Pineapple", "Extra Cheese"], ["10 inch", "12 inch", "14 inch"]);
+
 function Pizza(toppings, size) {
   this.toppings = toppings;
   this.size = size;
@@ -24,6 +26,13 @@ Pizza.prototype.priceCalc = function(pizza) {
 // ui logic
 
 $(document).ready(function() {
+  menu.sizes.reverse().forEach(function(size){
+    $("#pizza-form").prepend('<input type="radio" name="size" value="'+size+'">'+'<label>'+size+'</label>');
+  });
+  menu.toppings.reverse().forEach(function(topping){
+    $("#pizza-form").prepend('<input type="checkbox" name="toppings" value="'+topping+'">'+'<label>'+topping+'</label>');
+  });
+
   $("#pizza-form").submit(function(event){
     event.preventDefault();
     let size = $("input:radio[name=size]:checked").val();
